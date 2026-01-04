72c0e5b6e8e9c6011aebcc70758322cfe569e673
Noah <44441682+TheHarmonicRealm@users.noreply.github.com>
2026-01-04 11:14:44 -0800

    Preserve Verilator error colors over pipe

--- python/server__manager.py
+++ python/server__manager.py
@@ -116,9 +116,12 @@ def try_make(files: list[NamedFile]):
     # List is passed in as an environment variable
     filenames_str = " ".join([f"./user_inputs/{name}" for name in names])
     # Must append to existing environment or Verilator fails
-    envvars = environ.copy() | {"COMPILE_FILES": filenames_str}
+    envvars = environ.copy() | {"COMPILE_FILES": filenames_str, "CXXFLAGS": "-fdiagnostics-color"}
 
-    proc = subprocess.run(["make"], stderr=subprocess.PIPE, env=envvars)
+    # This and the CXXFLAGS make it so that errors' colors are preserved; the
+    #   commands otherwise know they are not in a terminal and strip them
+    #   Not sure why the env var is also needed (in real terminal it isn't)
+    proc = subprocess.run(["/bin/bash", "-c", "'make'"], stderr=subprocess.PIPE, env=envvars)
 
     match proc.returncode:
         case 0:
