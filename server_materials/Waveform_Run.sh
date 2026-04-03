545f736ccf3ca72e998e4e78e7073a7b2cfa2f32
Noah <44441682+TheHarmonicRealm@users.noreply.github.com>
2026-04-03 12:36:42 -0400

    Fix clashing NULLPORT-PINMISSING errors (does not make images incompatible)

--- server_materials/Waveform_Run.sh
+++ server_materials/Waveform_Run.sh
@@ -1,3 +1,5 @@
 set -e # Quit if step 1 fails
+# see Makefile lint_flags definition for some explanation
+verilator --lint-only --timing -Werror-NULLPORT -I./user_inputs $COMPILE_FILES
 verilator --binary --timing --trace -I./user_inputs $COMPILE_FILES
 ./obj_dir/Vtb
